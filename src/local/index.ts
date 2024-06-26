853114cc364425d9a054824647ff70934eb3410d
shunn2 <chsida655@gmail.com>
2024-06-26 18:15:01 +0900

    feat: retrieve를 통해 api 콜 및 value 캐싱 작업

--- src/local/index.ts
+++ src/local/index.ts
@@ -1,32 +1,54 @@
-import {
-  clearLocalStorage,
-  getLocalStorage,
-  removeLocalStorage,
-  setLocalStorage,
-} from '@utils/localStorage';
+import LocalStorageService from '@utils/localStorage';
+import { LocalCacheOption } from 'types';
 
 export default class LocalCache {
+  protected localStorageService: LocalStorageService =
+    new LocalStorageService();
+
   public getItem<T>(key: string): T | null {
-    const value = getLocalStorage<T>(key);
+    const value = this.localStorageService.getLocalStorage<T>(key);
 
     return value;
   }
 
   public setItem<T>(key: string, value: T): boolean {
-    const result = setLocalStorage<T>(key, value);
+    const result = this.localStorageService.setLocalStorage<T>(key, value);
 
     return result;
   }
 
   public removeItem(key: string): boolean {
-    const result = removeLocalStorage(key);
+    const result = this.localStorageService.removeLocalStorage(key);
 
     return result;
   }
 
   public clear(): boolean {
-    const result = clearLocalStorage();
+    const result = this.localStorageService.clearLocalStorage();
 
     return result;
+  }
+
+  public async retrieve<T, P extends Array<unknown>>(
+    options: LocalCacheOption<T, P>
+  ): Promise<T> {
+    const { key, expireOption, fn, fnProps = [] as unknown as P } = options;
+    const expiredKey = key + '_EXPIRED';
+    const isExpired = this.localStorageService.isExpired(
+      expiredKey,
+      expireOption
+    );
+
+    const value = this.getItem<T>(key);
+
+    if (isExpired || !value) {
+      const result = await fn(...fnProps);
+      this.setItem(key, result);
+      this.setItem(expiredKey, new Date().getTime());
+
+      return result;
+    }
+
+    return value;
   }
 }
