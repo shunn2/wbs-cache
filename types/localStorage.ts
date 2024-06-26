4568d7adaf6785a81a5453860844e4d9dee90057
shunn2 <chsida655@gmail.com>
2024-06-26 18:15:16 +0900

    fix: class로 객체 구조 변경

--- types/localStorage.ts
+++ types/localStorage.ts
@@ -1,4 +1,10 @@
 export interface ExpireOption {
-  key: string;
   validTime?: number;
+}
+
+export interface LocalCacheOption<T, P extends Array<unknown>> {
+  key: string;
+  expireOption: ExpireOption;
+  fn: (...args: P) => Promise<T>;
+  fnProps?: P;
 }
--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -2,66 +2,68 @@ import { ExpireOption } from 'types';
 
 export const DEFAULT_EXPIRE_TIME = 1000 * 60 * 30; // 30 minutes
 
-export const isExpired = (expireOption: ExpireOption): boolean => {
-  const expireTime = getLocalStorage<number>(expireOption.key);
+export default class LocalStorageService {
+  public isExpired = (key: string, expireOption?: ExpireOption): boolean => {
+    const expireTime = this.getLocalStorage<number>(key);
 
-  if (expireTime) {
-    const currentTime = new Date().getTime();
-    const validTime = expireOption.validTime ?? DEFAULT_EXPIRE_TIME;
-    return currentTime - validTime > Number(expireTime);
-  }
-
-  return true;
-};
-
-export const getLocalStorage = <T>(key: string): T | null => {
-  try {
-    const serializedValue = localStorage.getItem(key);
-    if (serializedValue === null) {
-      return null;
+    if (expireTime) {
+      const currentTime = new Date().getTime();
+      const validTime = expireOption?.validTime ?? DEFAULT_EXPIRE_TIME;
+      return currentTime - validTime > Number(expireTime);
     }
 
-    return JSON.parse(serializedValue) as T;
-  } catch (e) {
-    console.error('Error getting data from localStorage', e);
-
-    return null;
-  }
-};
-
-export const setLocalStorage = <T>(key: string, value: T): boolean => {
-  try {
-    const serializedValue = JSON.stringify(value);
-    localStorage.setItem(key, serializedValue);
-
     return true;
-  } catch (e) {
-    console.error('Error saving to localStorage', e);
+  };
 
-    return false;
-  }
-};
+  public getLocalStorage = <T>(key: string): T | null => {
+    try {
+      const serializedValue = localStorage.getItem(key);
+      if (serializedValue === null) {
+        return null;
+      }
 
-export const removeLocalStorage = (key: string): boolean => {
-  try {
-    localStorage.removeItem(key);
+      return JSON.parse(serializedValue) as T;
+    } catch (e) {
+      console.error('Error getting data from localStorage', e);
 
-    return true;
-  } catch (e) {
-    console.error('Error removing data from localStorage', e);
+      return null;
+    }
+  };
 
-    return false;
-  }
-};
+  public setLocalStorage = <T>(key: string, value: T): boolean => {
+    try {
+      const serializedValue = JSON.stringify(value);
+      localStorage.setItem(key, serializedValue);
 
-export const clearLocalStorage = (): boolean => {
-  try {
-    localStorage.clear();
+      return true;
+    } catch (e) {
+      console.error('Error saving to localStorage', e);
 
-    return true;
-  } catch (e) {
-    console.error('Error clearing data at localStorage', e);
+      return false;
+    }
+  };
 
-    return false;
-  }
-};
+  public removeLocalStorage = (key: string): boolean => {
+    try {
+      localStorage.removeItem(key);
+
+      return true;
+    } catch (e) {
+      console.error('Error removing data from localStorage', e);
+
+      return false;
+    }
+  };
+
+  public clearLocalStorage = (): boolean => {
+    try {
+      localStorage.clear();
+
+      return true;
+    } catch (e) {
+      console.error('Error clearing data at localStorage', e);
+
+      return false;
+    }
+  };
+}
